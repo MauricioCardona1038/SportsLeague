3fadac8c133a534f6d1c6acfb547cc4604131f64
Mauricio Cardona <mc573172@gmail.com>
2026-05-23 09:50:20 -0500

    Revert auto-match generation and remove Matches DbSet

--- SportsLeague.DataAccess/Context/LeagueDbContext.cs
+++ SportsLeague.DataAccess/Context/LeagueDbContext.cs
@@ -18,7 +18,6 @@ namespace SportsLeague.DataAccess.Context
         public DbSet<TournamentTeam> TournamentTeams => Set<TournamentTeam>();
         public DbSet<Sponsor> Sponsors => Set<Sponsor>();
         public DbSet<TournamentSponsor> TournamentSponsors => Set<TournamentSponsor>();
-        public DbSet<Match> Matches => Set<Match>();
         public DbSet<MatchResult> MatchResults => Set<MatchResult>();
 
         public DbSet<Goal> Goals => Set<Goal>();
